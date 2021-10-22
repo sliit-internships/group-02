e05e57c7392fa41acfe1ead6b980cd12d95016b2
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2021-10-22 22:36:21 +0530

    error fix

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api/', require('./routes/UserRoutes'));
+app.use('/api', require('./routes/UserRoutes'));
 
 const PORT = process.env.PORT || 5000;
 
