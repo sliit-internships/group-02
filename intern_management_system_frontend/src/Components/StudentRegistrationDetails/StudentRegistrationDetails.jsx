0f9ee603ec5bf208c09bcb0d25f33356c8f7a8b2
Prashani <aruniprashani@gmail.com>
2021-12-13 10:28:22 +0530

    Created Student Registration Details page

--- intern_management_system_frontend/src/Components/StudentRegistrationDetails/StudentRegistrationDetails.jsx
+++ intern_management_system_frontend/src/Components/StudentRegistrationDetails/StudentRegistrationDetails.jsx
@@ -2,61 +2,97 @@ import React from "react";
 import "./StudentRegistrationDetails.css"
 
 
+
 const StudentRegistrationDetails = (props) => {
     return (
-        <div>
-            <table className="table table-striped">
+        <div >
+            <img className="srd_image" src="https://preview.colorlib.com/theme/edumark/img/banner/bradcam.png.webp"/>
+            <h1 className="srd_h1">INTERNSHIP REGISTRATION DETAILS</h1>
+
+            <p className="srd_p">STUDENT DETAILS</p>
+
+            <table className="table srd_tableStyle">
+
+
                 <thead>
-                <tr>
-                    <th scope="col"></th>
-                    <th scope="col">ID</th>
-                    <th scope="col">Student Name</th>
-                    <th scope="col">Current Year</th>
-                    <th scope="col">planning to complete second year(Year)</th>
-                    <th scope="col">planning to complete second year(Period)</th>
+
+
+                <tr className="srt_tr">
+
+                    <th scope="col"> ID</th>
+                    <th scope="col"> Name</th>
+                    <th scope="col">Email</th>
                     <th scope="col">Specialization</th>
-                    <th scope="col">Mobile Number</th>
-                    <th scope="col">Home Number</th>
-                    <th scope="col">Intern Started Date </th>
-                    <th scope="col">Supervisor E-Mail</th>
-                    <th scope="col">Student E-Mail</th>
-                    <th scope="col">Status</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Start Date</th>
+                    <th scope="col">End Date</th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
 
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
+                    <td>IT12345678</td>
+                    <td>Perera P.K.L</td>
+                    <td>it12345678@my.sliit.lk</td>
+                    <td>SE</td>
+                    <td>Full stack software development</td>
+                    <td>2020/01/05</td>
+                    <td>2020/06/05</td>
                 </tr>
 
                 </tbody>
             </table>
 
+            <p className="srd_p">SUPERVISOR DETAILS</p>
+
+            <table className="table srd_tableStyle">
+                <thead>
+
+
+                <tr className="srt_tr">
+
+                    <th scope="col">Name</th>
+                    <th scope="col">Company</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Company Address</th>
+
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+
+                    <td>Bandara A.K.I</td>
+                    <td>Virtusa Pvt. Ltd.</td>
+                    <td>bandara@gmail.com</td>
+                    <td>Software Engineer</td>
+                    <td>752 Dr Danister De Silva Mawatha, Colombo</td>
+
+                </tr>
+
+                </tbody>
+            </table>
+
+            <table className="table srd_tableStyle">
+                <thead>
+
+                <tr className="srt_tr">
+
+                    <th scope="col" className="srd_tableText">Internship Manager Status</th>
+
+
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+
+                    <td className="srd_tableText">Pending</td>
+
+
+                </tr>
+
+                </tbody>
+            </table>
         </div>
 
 
