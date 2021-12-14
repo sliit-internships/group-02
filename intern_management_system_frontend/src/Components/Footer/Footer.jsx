1ddd5b711a7f0de0d14c61eebb584a70d6d5f99a
Prashani <aruniprashani@gmail.com>
2021-12-14 17:08:50 +0530

    Designed footer

---
+++ intern_management_system_frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,207 @@
+import React from "react";
+import "./Footer.css"
+
+
+const Footer = (props) => {
+    return (
+        <div>
+            <div className="page-wrapper">
+                <div id="waterdrop"></div>
+                <footer>
+                    <div className="footer-top">
+                        <div className="pt-exebar">
+
+                        </div>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-lg-3 col-md-12 col-sm-12 footer-col-3">
+                                    <div className="widget footer_widget">
+                                        <h5 className="footer-title">Address</h5>
+                                        <div className="gem-contacts">
+                                            <div className="gem-contacts-item gem-contacts-address">Corporate Office
+                                                :<br/> Doon House, B-275(A), First floor
+                                                    Sector-57, Shushant Lok 3
+                                                    Near Hong Kong Bazzar, Gurugram Pin 122001, Haryana.
+                                            </div>
+                                            <div className="gem-contacts-item gem-contacts-phone"><i
+                                                className="fa fa-phone" aria-hidden="true"></i> Phone: <a
+                                                href="">+91-9122588799</a></div>
+
+                                            <div className="gem-contacts-item gem-contacts-address mt-2">Reg. Office
+                                                :<br/> Doon House, D2/3,
+                                                    4th Floor, Chandra Tower, IDBI Bank Building
+                                                    Dimna Road, Mango, Jamshedpur-831012, Jharkhand.</div>
+                                            <div className="gem-contacts-item gem-contacts-phone"><i
+                                                className="fa fa-phone" aria-hidden="true"></i> Phone: <a>+91
+                                                9122588799</a></div>
+
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div className="col-12 col-lg-6 col-md-6 col-sm-12">
+                                    <div className="row">
+                                        <div className="col-6 col-lg-6 col-md-6 col-sm-6">
+                                            <div className="widget footer_widget">
+                                                <h5 className="footer-title">Recent News</h5>
+                                                <ul className="posts  styled">
+                                                    <li className="clearfix gem-pp-posts">
+                                                        <div className="gem-pp-posts-text">
+                                                            <div className="gem-pp-posts-item">
+                                                                <a href="#">Want to start Your Own Play School.
+                                                                </a>
+                                                            </div>
+                                                            <div className="gem-pp-posts-date">Call +91-9122588799</div>
+                                                        </div>
+                                                    </li>
+                                                    <li className="clearfix gem-pp-posts">
+                                                        <div className="gem-pp-posts-text">
+                                                            <div className="gem-pp-posts-item">
+                                                                <a href="#">Now we are in Faridabad,Now we are in
+                                                                    DaudNagar.
+                                                                </a>
+                                                            </div>
+                                                            <div className="gem-pp-posts-date">Call +91-9122588799,
+                                                                +91-9122588799
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                    <li className="clearfix gem-pp-posts">
+                                                        <div className="gem-pp-posts-text">
+                                                            <div className="gem-pp-posts-item">
+                                                                <a href="">Now we are in Ranchi, Admission open
+                                                                </a>
+                                                            </div>
+                                                            <div className="gem-pp-posts-date">Call +91-9122588799,
+                                                                +91-9122588799
+                                                            </div>
+                                                        </div>
+                                                    </li>
+
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div className="col-6 col-lg-6 col-md-6 col-sm-6">
+                                            <div className="widget">
+                                                <h5 className="footer-title">Email Us</h5>
+                                                <div className="textwidget">
+                                                    <div role="form" className="wpcf7" id="wpcf7-f4-o1" lang="en-US"
+                                                         dir="ltr">
+
+                                                        <form method="post" className="wpcf7-form"
+                                                              noValidate="novalidate">
+
+                                                            <div className="contact-form-footer">
+                                                                <p><span
+                                                                    className="wpcf7-form-control-wrap your-first-name"><input
+                                                                    type="text" name="your-first-name" value=""
+                                                                    size="40" className="wpcf7-form-control wpcf7-text"
+                                                                    aria-invalid="false" placeholder="Your name"/></span>
+                                                                </p>
+                                                                <p><span
+                                                                    className="wpcf7-form-control-wrap your-email_1"><input
+                                                                    type="email" name="your-email_1" value="" size="40"
+                                                                    className="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-email"
+                                                                    aria-invalid="false"
+                                                                    placeholder="Your email"/></span></p>
+                                                                <p><span
+                                                                    className="wpcf7-form-control-wrap your-message"><textarea
+                                                                    name="your-message" cols="40" rows="10"
+                                                                    className="wpcf7-form-control wpcf7-textarea"
+                                                                    aria-invalid="false"
+                                                                    placeholder="Your message"/></span></p>
+                                                                <div><input type="submit" value="Send"
+                                                                            className="wpcf7-form-control wpcf7-submit"/><span
+                                                                    className="ajax-loader"/></div>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-12 col-lg-3 col-md-5 col-sm-12 footer-col-4">
+                                    <div className="widget widget_gallery gallery-grid-4">
+                                        <h5 className="footer-title">Our Gallery</h5>
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum></p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="footer-bottom">
+                        <div className="container">
+                            <div className="row">
+
+                                <div className="col-md-3">
+                                    <div className="footer-site-info">2020 © <a
+                                        href="https://www.youtube.com/watch?v=pdr4IzBpacI&t=34s" target="_blank">Top
+                                        HTML & CSS Program </a></div>
+                                </div>
+
+                                <div className="col-md-6">
+                                    <nav id="footer-navigation"
+                                         className="site-navigation footer-navigation centered-box" role="navigation">
+                                        <ul id="footer-menu" className="nav-menu styled clearfix inline-inside">
+                                            <li id="menu-item-26"
+                                                className="menu-item menu-item-type-custom menu-item-object-custom menu-item-26">
+                                                <a href="#">Support</a></li>
+                                            <li id="menu-item-27"
+                                                className="menu-item menu-item-type-custom menu-item-object-custom menu-item-27">
+                                                <a href="#">Contact Us</a></li>
+                                            <li id="menu-item-28"
+                                                className="menu-item menu-item-type-custom menu-item-object-custom menu-item-28">
+                                                <a href="#">Disclaimer</a></li>
+                                            <li id="menu-item-29"
+                                                className="menu-item menu-item-type-custom menu-item-object-custom menu-item-29">
+                                                <a href="#">Add more</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+
+                                <div className="col-md-3">
+                                    <div id="footer-socials">
+                                        <div className="socials inline-inside socials-colored">
+
+                                            <a href="#" target="_blank" title="Facebook" className="socials-item">
+                                                <i className="fab fa-facebook-f facebook"></i>
+                                            </a>
+                                            <a href="#" target="_blank" title="Twitter" className="socials-item">
+                                                <i className="fab fa-twitter twitter"></i>
+
+                                            </a>
+                                            <a href="#" target="_blank" title="Instagram" className="socials-item">
+                                                <i className="fab fa-instagram instagram"></i>
+                                            </a>
+                                            <a href="#" target="_blank" title="YouTube" className="socials-item">
+                                                <i className="fab fa-youtube youtube"></i>
+                                            </a>
+                                            <a href="#" target="_blank" title="Telegram" className="socials-item">
+                                                <i className="fab fa-telegram telegram"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+
+
+        </div>
+
+
+
+
+
+
+
+)
+    ;
+};
+
+export default Footer;
